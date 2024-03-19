f42f308b819933f77a8b612e1e7d7b8c44f0f638
phanquoc123 <phanquoc1512@gmail.com>
2024-03-19 20:35:09 +0700

    quoc chia bo cuc phan view

---
+++ view/index.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.php';
+
+
+if(isset($_GET['act'])){
+    $act = $_GET['act'];
+    switch($act){
+        case 'chitiet_sanpham':
+            include 'chitietsanpham.php';
+            break;
+    }
+}else{
+    include 'slide_show.php';
+    include 'trangchu.php';
+
+}
+
+include 'footer.php';
+
+?>
---
+++ view/slide_show.php
@@ -0,0 +1,21 @@
+<div class="slide_show container">
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="../img/banner-thoi-trang-nam.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="../img/banner-thoi-trang-hien-dai-dep-nhat_113857069.jpg" class="d-block w-100" alt="...">
+          </div>
+          
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+  </div>

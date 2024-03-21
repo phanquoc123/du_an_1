2819094a0f9e8e73671daa87d5e1ce232e5773cb
anhnvph45648 <anhnvph45648>
2024-03-21 07:38:24 +0700

    Vanh hoan thanh trang chi tiet sp

--- view/chitietsanpham.php
+++ view/chitietsanpham.php
@@ -10,6 +10,18 @@
                 <img src="../img/anhsanpham1.webp" alt="">
                 <img src="../img/anhsanpham2.webp" alt="">
             </div>
+            <div class="product-content-left-bottom">
+                <h2>Đặc tính nổi bật</h2>
+                <ul>
+                    <li><a href=""></a>Áo có cấu trúc 3 lớp chắc chắn</li>
+                    <li><a href=""></a>Thành phần:  100% NYLON</li>
+                    <li><a href=""></a>Khả năng giữ ấm tốt, cản gió hiệu quả, chống thấm nước vào bên trong</li>
+                    <li><a href=""></a>Màu sắc trang nhã, trẻ trung, phù hợp với nhiều tone da chị em</li>
+                    <li><a href=""></a>Form dáng dễ mặc, tối giản nhưng không đơn điệu</li>
+                    <li><a href=""></a>Thiết kế túi đựng nhỏ gọn, dễ dàng đựng các vật dụng nhỏ khác</li>
+                    <li><a href=""></a>Áo phao siêu nhẹ, chống tĩnh điện tốt</li>
+                </ul>
+    </div>
         </div>
         <div class="product-content-right">
             <div class="product-content-right-product-name">
@@ -20,43 +32,55 @@
                 <p>1.500.000<sup>đ</sup></p>
             </div>
             <div class="product-content-right-product-color">
-                <p><span style="font-weight: bold;">Màu sắc: </span>Xanh nhạt <span style="color: red;">*</span></p>
-                <div class="product-content-right-product-color-img">
-                    <img src="../img/anhmau.jpg" alt="">
+                <p><span style="font-weight: bold;">Màu sắc: </span>Xanh nhạt <span style="color: red;">*</span></p> 
+                <div class="product-content-right-product-color-imgs">
+                    <div class="product-content-right-product-color-img">
+                        <img src="../img/anhmau.jpg" alt="">
+                    </div>
+                    <div class="product-content-right-product-color-img">
+                        <img src="../img/anhmau2.jpg" alt="">
+                    </div>
+                    <div class="product-content-right-product-color-img">
+                        <img src="../img/anhmau3.jpg" alt="">
+                    </div>
+                    <div class="product-content-right-product-color-img">
+                        <img src="../img/anhmau4.jpg" alt="">
+                    </div>  
+                    <div class="product-content-right-product-color-img">
+                        <img src="../img/anhmau5.jpg" alt="">
+                    </div>
                 </div>
-                
+
             </div>
             <div class="product-content-right-product-size">
                 <p style="font-weight: bold;">Size</p>
-                <span>S</span>
-                <span>M</span>
-                <span>L</span>
-                <span>XL</span>
-                <span>XXL</span>
+                <span><p>S</p></span>
+                <span><p>M</p></span>
+                <span><p>L</p></span>
+                <span><p>XL</p></span>
+                <span><p>XXL</p></span>
             </div>
             <div class="quantity">
                 <p style="font-weight: bold;">Số lượng: </p>
                 <input type="number" min="0" value="1">
             </div>
-            <p style="color: red;">Vui lòng chọn size</p>
+            <p style="color: orange;">Mua thêm để được Freeship cho đơn từ 498K</p>
             <div class="product-content-right-product-button">
-                <button><i class="fas fa-shopping-cart"></i> <p class="font-p">MUA HÀNG</p></button>
-                <button><p>TÌM TẠI CỬA HÀNG</p></button>
+                <button><i class="fas fa-shopping-cart"></i> <p class="font-p">THÊM VÀO GIỎ HÀNG</p></button>
+                <button><p class="font2-p" >MUA NGAY</p></button>
             </div>
             <div class="product-content-right-product-icon">
                 <div class="product-content-right-product-icon-item">
-                    <i class="fas fa-phone-alt"></i> <p class="font-p">Hotline</p>
+                    <i class="fas fa-phone-alt"></i> <p>Mã giảm giá sẽ hiển thị trong giỏ hàng</p>
                 </div>
                 <div class="product-content-right-product-icon-item">
-                    <i class="fas fa-comments"></i> <p class="font-p">Chat</p>
+                    <i class="fas fa-comments"></i> <p>Thông tin được bảo mật và mã hóa</p>
                 </div>
                 <div class="product-content-right-product-icon-item">
-                    <i class="fas fa-envelope"></i> <p class="font-p">Mail</p>
+                    <i class="fas fa-envelope"></i> <p>Giao hàng: Từ 1 - 3 ngày</p>
                 </div>
-            </div>
-            <div class="product-content-right-bottom">
-                <div class="product-content-right-bottom-top">
-
+                <div class="product-content-right-product-icon-item">
+                    <i class="fas fa-envelope"></i> <p>Miễn phí đổi trả: tại 250+ cửa hàng trong 15 ngày</p>
                 </div>
             </div>
         </div>
@@ -66,23 +90,25 @@
 <style>
 .product-content{
     margin-top: 30px;
+    margin-left: 150px;
 }
 
 .product-content-left{
     flex: 1;
     width: 50%;
     display: flex;
-    align-items: center;
+    margin-top: 40px;
     
 }
 .product-content-left-big-img{
     width: 80%;
-    padding-right: 20px;
     display: flex;
     justify-content: space-between;
+
 }
 .product-content-left-big-img img{
     width: 100%;
+    height: 100%;
 }
 .product-content-left-smail-img{
     width: 20%;
@@ -92,10 +118,12 @@
     align-items: center;
 }
 .product-content-left-smail-img img{
-    margin: 14px 20px; /* Khoảng cách giữa các hình ảnh nhỏ */
+    margin: 10px; /* Khoảng cách giữa các hình ảnh nhỏ */
     width: 100%;
-    align-self: flex-start;
+    height: 110%;
+    margin-top: 1px;
 }
+
 .product-content-right{
     flex: 1;
     width: 50%;
@@ -105,6 +133,10 @@
     font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     font-size: 24px;
     margin-top: 40px;
+}
+h2{
+    margin: 0 0 20px 120px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
 .product-content-right-product-name p{
     color: #cccccc;
@@ -116,27 +148,40 @@
     font-weight: bold;
     font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
-.product-content-right-product-color img{
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
+.product-content-right-product-color-imgs{
+    display: flex;
 }
 .product-content-right-product-color-img{
    height: 40px;
    width: 40px;
-   border: 1px solid #333;
+   border: 1px solid #F8F8FF;
    display: flex;
    align-items: center;
    justify-content: center;
-   margin: 12px 0;
+   margin-right: 20px;
+}
+.product-content-right-product-color-img:hover{
+    border: 1px solid black;
 }
 .product-content-right-product-size span{
     display: inline-block;
     padding: 3px 6px;
-    border: 1px solid black;
+    border: none;
     margin: -10px 10px 20px 0;
     font-size: 12px;
     cursor: pointer;
+    outline: none;
+    height: 30px;
+    width: 40px;
+    background-color: #F8F8FF;
+}
+.product-content-right-product-size span p{
+    font-size: 15px;
+    text-align: center;
+}
+.product-content-right-product-size span:hover{
+    background-color: #EEE8AA;
+    color: #F8F8FF;
 }
 .quantity{
     display: flex;
@@ -148,7 +193,7 @@
     margin: 0 10px;
 }
 .product-content-right-product-button button{
-    width: 150px;
+    width: 400px;
     height: 40px;
     display: block;
     margin: 20px 0 12px;
@@ -173,25 +218,25 @@
     cursor: pointer;
 }
 .product-content-right-product-icon{
-    display: flex;
-}
-.product-content-right-product-icon-item{
-    display: flex;
-    margin-right: 12px;
-    margin-top: 5px;
-    align-items: center;
-}
-.product-content-right-product-icon-item{
-    display: flex;
-    margin-right: 12px;
-    align-items: center;
+    display: block;
 }
 .product-content-right-product-icon-item i{
     font-size: 12px;
     margin-right: 6px;
 }
 .font-p{
-    padding-top: 12px;
+    padding: 20px 55px 0 0;
 }
+.font2-p{
+    padding-top: 5px;
+}
+.product-content-right-product-icon-item p{
+    padding-top: 16px;
+}
+.product-content-left-bottom{
+    padding-top: 250px;
+}
+
+
 
 </style>
